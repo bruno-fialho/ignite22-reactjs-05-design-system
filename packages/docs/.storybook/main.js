73d9e0a60ca7eebba1dabe407fae0799155d2373
Bruno Fialho <brugerf@gmail.com>
2023-01-07 10:47:57 -0300

    feat: add subpath to storybook on prod

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,5 +15,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite22-reactjs-05-design-system/'
+    }
+
+    return config
   }
 }
